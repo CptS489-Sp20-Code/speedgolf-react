9ed8d4f363da6a45ff864642713dc5ab4b3e3176
Chris Hundhausen <chundhau@gmail.com>
2020-02-03 14:04:30 -0800

    hideModeBarOnLogin

--- src/components/ModeBar.js
+++ src/components/ModeBar.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import AppMode from '../AppMode.js';
 
 class ModeBar extends React.Component {
     render() {
       return(
-        <div id ="bottomBar" className="modebar">
+        <div className={"modebar" + (this.props.mode === AppMode.LOGIN ? " invisible" : " visible")}>
         <a className="modebar-btn">
           <span className="modebaricon fa fa-th-list"></span>
           <span className="modebar-text">Feed</span>
         </a>
-        <a className="modebarbtn">
+        <a className="modebar-btn">
           <span className="modebar-icon  fa fa-history"></span>
           <span className="modebar-text">Rounds</span>
         </a>
-        <a className="modebarbtn">
+        <a className="modebar-btn">
           <span className="modebar-icon  fa fa-flag"></span>
           <span className="modebar-text">Courses</span>
         </a> 
