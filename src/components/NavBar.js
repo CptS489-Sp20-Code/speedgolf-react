ef2c282e5202ab1ab9c6f41869f1dad69ac9edad
Chris Hundhausen <chundhau@gmail.com>
2020-02-04 18:49:01 -0800

    navbar render method updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import AppMode from '../AppMode.js';
 
 class NavBar extends React.Component {
     
+    handleMenuBtnClick = () => {
+      if (this.props.mode != AppMode.LOGIN) {
+        this.props.toggleMenuOpen();
+      }
+    }
+
     render() {
        return (
         <div className="navbar">  
         <span className="navbar-items">
-          <button className="sidemenu-btn">
-            <span id="menuBtnIcon" className="fa fa-bars sidemenu-btn-icon">
+          <button className="sidemenu-btn" onClick={this.handleMenuBtnClick}>
+            <span className={"sidemenu-btn-icon " + 
+                (this.props.menuOpen ? "fa fa-times":"fa fa-bars")}>
             </span>
           </button>
           <img src="http://tiny.cc/sslogo" alt="Speed Score Logo" height="38px"
