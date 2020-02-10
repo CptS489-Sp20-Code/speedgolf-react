d9339f950f8f9eacbb51afe506f3094ff6edfa3d
Chris Hundhausen <chundhau@gmail.com>
2020-02-10 13:47:56 -0800

    createNewAccount1

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,8 +7,16 @@ class LoginPage extends React.Component {
         super(props);
         //Create a ref for the email input DOM element
         this.emailInputRef = React.createRef();
+        this.repeatPassRef = React.createRef();
         this.state = {loginBtnIcon: "fa fa-sign-in",
-                    loginBtnLabel: "Log In"};
+                      loginBtnLabel: "Log In",
+                      showAccountDialog: false,
+                      showPasswordDialog: false,
+                      accountName: "",
+                      accountPassword: "",
+                      accountPasswordRepeat: "",
+                      accountSecurityQuestion: "",
+                      accountSecurityAnswer: ""};
     }
 
     //Focus cursor in email input field when mounted
@@ -16,40 +24,157 @@ class LoginPage extends React.Component {
         this.emailInputRef.current.focus();
     }
 
-//handleLogin -- Callback function that sets up initial app state upon login.
-handleLogin = () => {
-    this.setState({loginBtnIcon: "fa fa-sign-in",
-                   loginBtnLabel: "Log In"});
-    //Set the userId of current user
-    let thisUser = this.emailInputRef.current.value;
-    this.props.setUserId(thisUser);
-    //Check whether we have saved data on this (or any) user:
-    let data = localStorage.getItem("speedgolfUserData");
-    if (data == null) { 
-      //No user app data stored yet -- create blank record for current user
-      localStorage.setItem("speedgolfUserData",
-      JSON.stringify({[thisUser] : {"rounds" : {}, "roundCount": 0}}));  
-    } else {
-      //app data exists -- check if data exists for thisUser
-      data = JSON.parse(data);
-      if  (!data.hasOwnProperty(thisUser)) { 
-          //No data for this user -- create empty data
-          data[thisUser] = {"rounds": {}, "roundCount": 0}; 
-          localStorage.setItem("speedgolfUserData",JSON.stringify(data));
-      } 
-    }  
-    //Trigger switch to FEED mode
-    this.props.changeMode(AppMode.FEED);
+    //handleLoginSubmit -- Called when user clicks on login button. Initiate spinner
+    //for 1 second and call handleLogin to do the work.
+    handleLoginSubmit = (event) => {
+        this.props.setUserId(this.emailInputRef.current.value);
+        //Trigger switch to FEED mode (default app landing page)
+        this.props.changeMode(AppMode.FEED);
+    }
+
+
+    //checkAccountValidity -- Callback function invoked after a form element in
+    //the 'Create Account' dialog box changes and component state has been
+    //updated. We need to check whether the passwords match and whether an
+    //account is already associated with the email address entered. If so, we
+    //set a custom validity message to be displayed when the user clicks the
+    //'Create Account' button. Otherwise, we reset the custom validity message
+    //to empty so that it will NOT fire when the user clicks 'Create Account'.
+    checkAccountValidity = () => {
+        if (this.state.accountPassword != this.state.accountPasswordRepeat) {
+            //Passwords don't match
+            this.repeatPassRef.current.setCustomValidity("This password must match original password.");
+        } else {
+            this.repeatPassRef.current.setCustomValidity("");
+        }
+    }
+    
+    //handleNewAccountChange -- Called when a field in a dialog box form changes.
+    //Update corresponding state variable and potentially update the custom
+    //message.
+    handleNewAccountChange = (event) => {
+        this.setState({[event.target.name]: event.target.value},this.checkAccountValidity);
+    }
+
+    //handleCreateAccount -- Triggered when user clicks on "Create Account."
+    //Custom data checking ensures user account under this email does not exist
+    //and that the rest of the info is valid. At this point, we can create 
+    //new object for user, save to localStorage and take user to app's landing page. 
+    handleCreateAccount = (event) => {
+        event.preventDefault();
+        //TO DO: update local storage to create account
+        //Set current user
+        this.props.setUserId(this.state.accountName);
+        //Log in user by switching to FEED mode
+        this.props.changeMode(AppMode.FEED);
+    }
+        
+    //renderAccountDialog -- Present the "create account" dialog
+    renderAccountDialog = () => {
+        return (
+        <div className="modal" role="dialog">
+          <div className="modal-dialog modal-lg">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h3 className="modal-title"><b>Create New Account</b>
+                  <button className="close-modal-button" 
+                    onClick={() => {this.setState({showAccountDialog: false})}}>
+                    &times;</button>
+                </h3>
+              </div>
+              <div className="modal-body">
+                <form onSubmit={this.handleCreateAccount}>
+                <label>
+                    Email: 
+                    <input
+                    className="form-control form-text"
+                    name="accountName"
+                    type="email"
+                    size="35"
+                    placeholder="Enter Email Address"
+                    pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}"
+                    required={true}
+                    value={this.state.accountName}
+                    onChange={this.handleNewAccountChange}
+                    />
+                </label>
+                
+                <label>
+                    Password:
+                    <input
+                    className="form-control form-text"
+                    name="accountPassword"
+                    type="password"
+                    size="35"
+                    placeholder="Enter Password"
+                    pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
+                    required={true}
+                    ref={this.repeatPassRef}
+                    value={this.state.accountPassword}
+                    onChange={this.handleNewAccountChange}
+                    />
+                </label>
+                
+                <label>
+                    Repeat Password:
+                    <input
+                    className="form-control form-text"
+                    name="accountPasswordRepeat"
+                    type="password"
+                    size="35"
+                    placeholder="Repeat Password"
+                    required={true}
+                    ref={this.repeatPassRef}
+                    value={this.state.accountPasswordRepeat}
+                    onChange={this.handleNewAccountChange}
+                    />
+                </label>
+                
+                <label>
+                    Security Question:
+                    <textarea
+                    className="form-control form-text"
+                    name="accountSecurityQuestion"
+                    size="35"
+                    placeholder="Security Question"
+                    rows="2"
+                    cols="35"
+                    maxLength="100"
+                    required={true}
+                    value={this.state.accountSecurityQuestion}
+                    onChange={this.handleNewAccountChange}
+                    />
+                </label>
+                <label>
+                    Answer to Security Question:
+                    <textarea
+                    className="form-control form-text"
+                    name="accountSecurityAnswer"
+                    type="text"
+                    placeholder="Answer"
+                    rows="2"
+                    cols="35"
+                    maxLength="100"
+                    required={true}
+                    value={this.state.accountSecurityAnswer}
+                    onChange={this.handleNewAccountChange}
+                    />
+                </label>
+                <button role="submit" className="btn btn-primary btn-color-theme form-submit-btn">
+                    <span className="fa fa-user-plus"></span>&nbsp;Create Account
+                </button>
+                </form>
+            </div>
+          </div>
+        </div>
+    </div>
+    );
+
 }
 
-//handleSubmit -- Called when user clicks on login button. Initiate spinner
-//for 1 second and call handleLogin to do the work.
-handleSubmit = (event) => {
-this.setState({loginBtnIcon: "fa fa-spin fa-spinner",
-                loginBtnLabel: "Logging In..."});
-//Initiate spinner for 1 second
-setTimeout(this.handleLogin,1000);
-event.preventDefault();
+//renderPasswordDialog -- Present the 'reset password' dialog
+renderPasswordDialog = () => {
+    
 }
 
 render() {
@@ -57,7 +182,7 @@ render() {
     <div id="login-mode-div" className="padded-page">
     <center>
         <h1 />
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleLoginSubmit}>
         <label htmlFor="emailInput" style={{ padding: 0, fontSize: 24 }}>
             Email:
             <input
@@ -89,6 +214,12 @@ render() {
             &nbsp;{this.state.loginBtnLabel}
         </button>
         <br />
+        <p><button type="button" className="btn btn-link login-link" 
+             onClick={() => {this.setState({showAccountDialog: true});}}>Create an account</button> | 
+           <button type="button" className="btn btn-link login-link"
+             onClick={() => {this.setState({showPasswordDialog: true});}}>Reset your password</button>
+        </p>
+     
         <a role="button" className="login-btn">
             <img src="https://drive.google.com/uc?export=view&id=1YXRuG0pCtsfvbDSTzuM2PepJdbBpjEut" />
         </a>
@@ -102,6 +233,8 @@ render() {
             <i>© 2020 Professor of Speedgolf. All rights reserved.</i>
         </p>
         </form>
+        {this.state.showAccountDialog ? this.renderAccountDialog() : null}
+        {this.state.showPasswordDialog ? this.renderPasswordDialog() : null}
     </center>
     </div>
     )
