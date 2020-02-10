9ada9927f6fb79f45096c1e60bf0840286262285
Chris Hundhausen <chundhau@gmail.com>
2020-02-10 14:10:39 -0800

    userAccountsComplete

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,6 +7,7 @@ class LoginPage extends React.Component {
         super(props);
         //Create a ref for the email input DOM element
         this.emailInputRef = React.createRef();
+        this.passwordInputRef = React.createRef();
         this.repeatPassRef = React.createRef();
         this.newUserRef = React.createRef();
         this.state = {loginBtnIcon: "fa fa-sign-in",
@@ -20,77 +21,110 @@ class LoginPage extends React.Component {
                       accountSecurityAnswer: ""};
     }
 
-    //Focus cursor in email input field when mounted
+//Focus cursor in email input field when mounted
     componentDidMount() {
         this.emailInputRef.current.focus();
-    }
+}
 
-    //handleLoginSubmit -- Called when user clicks on login button. Initiate spinner
-    //for 1 second and call handleLogin to do the work.
-    handleLoginSubmit = (event) => {
-        this.props.setUserId(this.emailInputRef.current.value);
-        //Trigger switch to FEED mode (default app landing page)
-        this.props.changeMode(AppMode.FEED);
-    }
+//handleLogin -- Callback function that sets up initial app state upon login.
+handleLogin = () => {
+    //Stop spinner
+    this.setState({loginBtnIcon: "fa fa-sign-in",
+                loginBtnLabel: "Log In"});
+    //Set current user
+    this.props.setUserId(this.emailInputRef.current.value);
+    //Trigger switch to FEED mode (default app landing page)
+    this.props.changeMode(AppMode.FEED);
+}
 
+//handleLoginSubmit -- Called when user clicks on login button. Initiate spinner
+//for 1 second and call handleLogin to do the work.
+handleLoginSubmit = (event) => {
+    event.preventDefault();
+    this.setState({loginBtnIcon: "fa fa-spin fa-spinner",
+                    loginBtnLabel: "Logging In..."});
+    //Initiate spinner for 1 second
+    setTimeout(this.handleLogin,1000);
+}
 
-    //checkAccountValidity -- Callback function invoked after a form element in
-    //the 'Create Account' dialog box changes and component state has been
-    //updated. We need to check whether the passwords match and whether an
-    //account is already associated with the email address entered. If so, we
-    //set a custom validity message to be displayed when the user clicks the
-    //'Create Account' button. Otherwise, we reset the custom validity message
-    //to empty so that it will NOT fire when the user clicks 'Create Account'.
-    checkAccountValidity = () => {
-        if (this.state.accountPassword != this.state.accountPasswordRepeat) {
-            //Passwords don't match
-            this.repeatPassRef.current.setCustomValidity("This password must match original password.");
-        } else {
-            this.repeatPassRef.current.setCustomValidity("");
-        }
-        let data = JSON.parse(localStorage.getItem("speedgolfUserData"));
-        if (data != null && data.hasOwnProperty(this.state.accountName)) {
-            //The user name is already taken
-            this.newUserRef.current.setCustomValidity("An account already exists under this email address. Use 'Reset password' to recover the password.");
-        } else {
-            this.newUserRef.current.setCustomValidity("");
-        }
+//checkAccountValidity -- Callback function invoked after a form element in
+//the 'Create Account' dialog box changes and component state has been
+//updated. We need to check whether the passwords match and whether an
+//account is already associated with the email address entered. If so, we
+//set a custom validity message to be displayed when the user clicks the
+//'Create Account' button. Otherwise, we reset the custom validity message
+//to empty so that it will NOT fire when the user clicks 'Create Account'.
+checkAccountValidity = () => {
+    if (this.state.accountPassword != this.state.accountPasswordRepeat) {
+        //Passwords don't match
+        this.repeatPassRef.current.setCustomValidity("This password must match original password.");
+    } else {
+        this.repeatPassRef.current.setCustomValidity("");
     }
+    let data = JSON.parse(localStorage.getItem("speedgolfUserData"));
+    if (data != null && data.hasOwnProperty(this.state.accountName)) {
+        //The user name is already taken
+        this.newUserRef.current.setCustomValidity("An account already exists under this email address. Use 'Reset password' to recover the password.");
+    } else {
+        this.newUserRef.current.setCustomValidity("");
+    }
+}
     
-    //handleNewAccountChange -- Called when a field in a dialog box form changes.
-    //Update corresponding state variable and potentially update the custom
-    //message.
-    handleNewAccountChange = (event) => {
-        this.setState({[event.target.name]: event.target.value},this.checkAccountValidity);
-    }
+//handleNewAccountChange -- Called when a field in a dialog box form changes.
+//Update corresponding state variable and potentially update the custom
+//message.
+handleNewAccountChange = (event) => {
+    this.setState({[event.target.name]: event.target.value},this.checkAccountValidity);
+}
 
-    //handleCreateAccount -- Triggered when user clicks on "Create Account."
-    //Custom data checking ensures user account under this email does not exist
-    //and that the rest of the info is valid. At this point, we can create 
-    //new object for user, save to localStorage and take user to app's landing page. 
-    handleCreateAccount = (event) => {
-        event.preventDefault();
-        let data = JSON.parse(localStorage.getItem("speedgolfUserData"));
-        //Create fresh user data object for new user
-        if (data == null) {
-            data = {}; //create empty data object
-        }
-        data[this.state.accountName] = {
-            accountInfo: {
-            password: this.state.accountPassword,
-            securityQuestion: this.state.accountSecurityQuestion,
-            securityAnswer: this.state.accountSecurityAnswer
-            },
-            rounds: {}, 
-            roundCount: 0
-        };
-        //Commit to localStorage:
-        localStorage.setItem("speedgolfUserData",JSON.stringify(data));
-        //Set current user
-        this.props.setUserId(this.state.accountName);
-        //Log in user by switching to FEED mode
-        this.props.changeMode(AppMode.FEED);
+//handleCreateAccount -- Triggered when user clicks on "Create Account."
+//Custom data checking ensures user account under this email does not exist
+//and that the rest of the info is valid. At this point, we can create 
+//new object for user, save to localStorage and take user to app's landing page. 
+handleCreateAccount = (event) => {
+    event.preventDefault();
+    let data = JSON.parse(localStorage.getItem("speedgolfUserData"));
+    //Create fresh user data object for new user
+    if (data == null) {
+        data = {}; //create empty data object
     }
+    data[this.state.accountName] = {
+        accountInfo: {
+        password: this.state.accountPassword,
+        securityQuestion: this.state.accountSecurityQuestion,
+        securityAnswer: this.state.accountSecurityAnswer
+        },
+        rounds: {}, 
+        roundCount: 0
+    };
+    //Commit to localStorage:
+    localStorage.setItem("speedgolfUserData",JSON.stringify(data));
+    //Set current user
+    this.props.setUserId(this.state.accountName);
+    //Log in user by switching to FEED mode
+    this.props.changeMode(AppMode.FEED);
+}
+
+//handleLoginChange -- Check the validity of the username (email address)
+//password entered into the login page, setting the customValidity message 
+//appropriately. 
+handleLoginChange = (event) => {
+    let thisUser = this.emailInputRef.current.value;
+    let data = JSON.parse(localStorage.getItem("speedgolfUserData"));
+    //Check username and password:
+    if (data == null || !data.hasOwnProperty(thisUser)) { 
+       this.emailInputRef.current.setCustomValidity("No account with this email address exists. Choose 'Create an account'.");
+         return; //Exit the function; no need to check pw validity
+     } else {
+         this.emailInputRef.current.setCustomValidity("");
+     }
+     if (data[thisUser].accountInfo.password != this.passwordInputRef.current.value) {
+        this.passwordInputRef.current.setCustomValidity("The password you entered is incorrect. Please try again or choose 'Reset your password'.");
+     } else {
+        this.passwordInputRef.current.setCustomValidity("");
+     }
+ }
+ 
         
     //renderAccountDialog -- Present the "create account" dialog
     renderAccountDialog = () => {
@@ -206,7 +240,7 @@ render() {
     <div id="login-mode-div" className="padded-page">
     <center>
         <h1 />
-        <form onSubmit={this.handleLoginSubmit}>
+        <form onSubmit={this.handleLoginSubmit} onChange={this.handleLoginChange}>
         <label htmlFor="emailInput" style={{ padding: 0, fontSize: 24 }}>
             Email:
             <input
@@ -223,6 +257,7 @@ render() {
         <label htmlFor="passwordInput" style={{ padding: 0, fontSize: 24 }}>
             Password:
             <input
+            ref={this.passwordInputRef}
             className="form-control login-text"
             type="password"
             placeholder="Enter Password"
